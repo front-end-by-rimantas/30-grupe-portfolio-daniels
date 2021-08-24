2c20608fa91a6b6f3838a9b531c9c2a0615a9289
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-08-24 22:19:56 +0300

    komentarai skirtingoms Sekcijoms

--- js/home.js
+++ js/home.js
@@ -1,3 +1,52 @@
 // import JS modules
 
 // execute JS modules
+
+/* HEADER START */
+
+/* HEADER END */
+
+
+/* HERO START */
+
+/* HERO END */
+
+
+/* ABOUT ME START */
+
+/* ABOUT ME END */
+
+
+/* SERVICES START */
+
+/* SERVICES END */
+
+
+/* PORTFOLIO START */
+
+/* PORTFOLIO END */
+
+
+/* TESTIMONIALS START */
+
+/* TESTIMONIALS END */
+
+
+/* HAPPY CUSTOMERS/COMPLETE PROJECTS...ACHIEVEMENTS START */
+
+/* HAPPY CUSTOMERS/COMPLETE PROJECTS...ACHIEVEMENTS END */
+
+
+/* BLOG START */
+
+/* BLOG END  */
+
+
+/* CONTACT US START */
+
+/* CONTACT US END */
+
+
+/* FOOTER START */
+
+/* FOOTER END */
