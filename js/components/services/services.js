21319562247b0c38773b468a2f45c0231fb76006
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-08-29 22:07:07 +0300

    services.js

--- js/components/services/services.js
+++ js/components/services/services.js
@@ -5,6 +5,8 @@ function services() {
     // 4 steps to follow for making a working function:
 
     // 1-input validation
+    console.log(selector);
+    console.log(data);
 
     // 2-logic
     let HTML = ''; /* kuriame funkcija, jos uzduotis sukurti HTML'ą ir jį kažkur įstatyti į tinkamą vietą. Norint sužinoti kur, turėsime gauti selector, duomenis(data), */
