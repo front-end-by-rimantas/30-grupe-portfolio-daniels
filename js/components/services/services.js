4088e4c04f05bfe2d5f7987249f8b7b20bd1fd78
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-08-30 09:14:02 +0300

    Selectors

--- js/components/services/services.js
+++ js/components/services/services.js
@@ -5,8 +5,15 @@ function services() {
     // 4 steps to follow for making a working function:
 
     // 1-input validation
-    console.log(selector);
-    console.log(data);
+    if (typeof selector !== 'string' || selector ==='') {
+        console.error('ERROR: nevalidus selector');
+        return false;
+    }
+
+    if(!Array.isArray(data) || data.length ===) {
+        console.error('ERROR: nevalidus data');
+        return false;
+    }
 
     // 2-logic
     let HTML = ''; /* kuriame funkcija, jos uzduotis sukurti HTML'ą ir jį kažkur įstatyti į tinkamą vietą. Norint sužinoti kur, turėsime gauti selector, duomenis(data), */
